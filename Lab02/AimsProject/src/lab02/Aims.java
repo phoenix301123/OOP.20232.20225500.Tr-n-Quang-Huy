c4dad4814c365dd6c79a7e95e839616a1619cb4e
phoenix301123 <tranhuylovechess@gmail.com>
2024-03-27 09:02:52 +0700

    Lab 02 assignment

--- Lab02/AimsProject/src/lab02/Aims.java
+++ Lab02/AimsProject/src/lab02/Aims.java
@@ -4,7 +4,7 @@ public class Aims {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		/*
+		
 		Cart anOrder = new Cart();
 		DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
 		anOrder.addDigitalVideoDisc(dvd1);
@@ -15,13 +15,12 @@ public class Aims {
 		System.out.println("Total Cost: ");
 		float totalCost = anOrder.totalCost();
 		System.out.println(totalCost);
-		/*
-		//int id1 = dvd1.getId();
-		//System.out.println("The first DVD's id: " + id1);
-		//int id2 = dvd2.getId();
-		//System.out.println("The second DVD's id: " + id2);
-		 * 
-		 */
+		
+		int id1 = dvd1.getId();
+		System.out.println("The first DVD's id: " + id1);
+		int id2 = dvd2.getId();
+		System.out.println("The second DVD's id: " + id2);
+		
 	}
 
 }
