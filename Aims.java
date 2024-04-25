6de3172e1335a73070079523da75860e5bebf6d4
Trần Quang Huy <145090977+phoenix301123@users.noreply.github.com>
2024-04-25 14:53:33 +0700

    Update Aims.java

--- Aims.java
+++ Aims.java
@@ -67,33 +67,33 @@ public class Aims {
         store.addMedia(dvd3);
 
     
-        Book book = new Book("The Valley of Fear", "Detective", 20.00f);
-        Book book1 = new Book("A Living Remedy: A Memoir", "Biography", 202.00f);
-        Book book2 = new Book("On the Origin of Time: Stephen Hawking's Final Theory", "Science", 120.00f);
+        Book book = new Book("S", 20.00f);
+        Book book1 = new Book("W", "Biography", 202.00f);
+        Book book2 = new Book("Q", "Science", 120.00f);
         store.addMedia(book);
         store.addMedia(book1);
         store.addMedia(book2);
 
 
         CompactDisc cd1 = new CompactDisc("Adele", "Music","Adele", 1500.98f);
-        Track track1CD1 = new Track("All Night Parking", 161);
-        Track track2CD1 = new Track("To Be Loved", 403);
-        Track track3CD1 = new Track("Woman Like Me", 300);
+        Track track1CD1 = new Track("X", 161);
+        Track track2CD1 = new Track("Y", 403);
+        Track track3CD1 = new Track("Z", 300);
         cd1.addTrack(track1CD1);
         cd1.addTrack(track2CD1);
         cd1.addTrack(track3CD1);
 
         CompactDisc cd2 = new CompactDisc("The Gods We Can Touch", "Music","Aurora", 2000.22f);
-        Track track1CD2 = new Track("Everything Matters", 180+34);
-        Track track2CD2 = new Track("Blood in the Wine", 180+30);
-        Track track3CD2 = new Track("Artemis", 60*2+39);
+        Track track1CD2 = new Track("D", 180+34);
+        Track track2CD2 = new Track("E", 180+30);
+        Track track3CD2 = new Track("F", 60*2+39);
         cd2.addTrack(track1CD2);
         cd2.addTrack(track2CD2);
         cd2.addTrack(track3CD2);
 
-        CompactDisc cd3 = new CompactDisc("Purpose", "Music","Justin Bieber", 1000.98f);
-        Track track1CD3 = new Track("The Feeling", 4*60+5);
-        Track track2CD3 = new Track("No Sense", 4*60+35);
+        CompactDisc cd3 = new CompactDisc("A", "Music","Justin Bieber", 1000.98f);
+        Track track1CD3 = new Track("B", 4*60+5);
+        Track track2CD3 = new Track("C", 4*60+35);
         cd3.addTrack(track1CD3);
         cd3.addTrack(track2CD3);
 
