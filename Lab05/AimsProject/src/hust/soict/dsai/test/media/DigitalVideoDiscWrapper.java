479654ea8b4829ae437eb9d42b833bccc79e6e9e
Trần Quang Huy <145090977+phoenix301123@users.noreply.github.com>
2024-06-02 15:01:57 +0700

    Lab05 assignment

---
+++ Lab05/AimsProject/src/hust/soict/dsai/test/media/DigitalVideoDiscWrapper.java
@@ -0,0 +1,11 @@
+package hust.soict.cybersec.test.media;
+import hust.soict.cybersec.aims.media.DigitalVideoDisc;
+
+public class DigitalVideoDiscWrapper {
+    DigitalVideoDisc dvd;
+    DigitalVideoDiscWrapper(DigitalVideoDisc dvd) {
+        super();
+
+        this.dvd = dvd;
+    }
+}
